78ed88979e990a64beb066ee79396feaccb899a4
CHNsPart <imchn24@gmail.com>
2021-09-20 16:31:39 +0600

    Initialize Voice Command

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const alanKey = '85afda625011d666d8ec3e9126927c1b2e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App = () => {
+
+    useEffect(()=>{
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command})=>{
+                if(command==="testCommand"){
+                    alert("This code was exc");
+                }
+            }
+        })
+    },[])
+
+    return (
+        <>
+            <h1 className="center">CHNsPart NewsApp powred by Alan-AI</h1>
+        </>
+    );
 }
 
 export default App;
